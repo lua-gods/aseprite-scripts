0aa587e994aaa5aa09f5f58eac5070acaa76a8ef
GNamimates <81744903+GNamimates@users.noreply.github.com>
2024-10-15 03:25:38 +0800

    anotha better normals dialog

--- Height Map to Normal Map.lua
+++ Height Map to Normal Map.lua
@@ -9,6 +9,7 @@
 
   --TODO
    - add a non linear strength filter to normalize the normals in the normal map
+   - bring back the bias slider somehow
   
 ------------------------------]]
 
@@ -72,24 +73,38 @@ app.transaction("Normal Map Preview",function ()
   preview_layer.name = "Preview"
   preview_layer.isEditable = false
 
-local offsets = {
-  {-1,-1},
-  {0,-1},
-  {1,-1},
-  {-1,0},
-  {1,0},
-  {-1,1},
-  {0,1},
-  {1,1},
+local kernels = {
+  ["Adjacents & Diagonals"] = {
+    {-1,-1},
+    {0,-1},
+    {1,-1},
+    {-1,0},
+    {1,0},
+    {-1,1},
+    {0,1},
+    {1,1},
+  },
+  ["Adjacents"] = {
+    {-1,0},
+    {1,0},
+    {0,1},
+    {0,-1},
+  },
+  ["Diagonals"] = {
+    {-1,-1},
+    {1,-1},
+    {-1,1},
+    {1,1},
+  }
 }
 
-local lookup = {
-    ["Always Top Left"] = {{-1,-1}},
-   ["Always Top Right"] = {{ 1,-1}},
- ["Always Bottom Left"] = {{-1, 1}},
-["Always Bottom Right"] = {{ 1, 1}},
-}
+local function lerp(a,b,w)
+  return a + (b - a) * w
+end
 
+local function clamp(v,min,max)
+  return math.min(math.max(v,min),max)
+end
 
 
 ---@param img Image
@@ -99,33 +114,34 @@ local lookup = {
 ---@return integer
 local function height(img,x,y)
   local r,g,b,a = rgba(clr(img,x,y))
-  return grayscale(r,g,b),a
+  return grayscale(r,g,b) / 255,a
 end
-
 
 ---@param img Image
 ---@param x integer
 ---@param y integer
----@param bias number
+---@param bias integer
 ---@param offsets {[1]:integer, [2]:integer}[]
 local function getNormal(img,x,y,offsets,bias)
   local nx,ny = 0,0
   local root_height,alpha = height(img,x,y)
   local contributed = 0
-  if alpha == 0 then return 0,0 end
   for i, o in pairs(offsets) do
     local snlen = math.sqrt(o[1]^2 + o[2]^2)
     local sample_height,sampled_alpha = height(img,x+o[1],y+o[2])
     if sampled_alpha ~= 0 then
       local delta = sample_height - root_height
-      contributed = contributed + delta * bias
+      if bias == -1 then delta = clamp(delta,-1,0)
+      elseif bias == 1 then delta = clamp(delta,0,1)
+      end
+      contributed = contributed + snlen
       nx = nx + o[1] / snlen * delta
       ny = ny + o[2] / snlen * delta
     end
   end
   local clen = math.max(contributed,1)
-  nx = math.max(math.min(nx / clen,254),-254)
-  ny = math.max(math.min(ny / clen,254),-254)
+  nx = nx / clen
+  ny = ny / clen
   
   return nx,ny
 end
@@ -147,23 +163,28 @@ local function updatePreview()
       for y = 0, target_img.height-1, 1 do
         for x = 0, target_img.width-1, 1 do
           
-          
-          local r,g,b,a = rgba(clr(target_img,x,y))
-          local main_height = grayscale(rgba(clr(target_img,x,y)))
+          local _,_,_,a = rgba(clr(target_img,x,y))
           local dx,dy
           
-          dx,dy = getNormal(target_img,x,y,offsets,dialog.data.bias / 10)
+---@diagnostic disable-next-line: param-type-mismatch
+          dx,dy = getNormal(target_img,x,y,kernels[dialog.data.kernel],dialog.data.bias or 0)
           
-          local strength = dialog.data.strength / 100
+          local s = dialog.data.normalize / 100
+          local f = (dialog.data.flip_y and -1 or 1)
           
-          dx = dx
-          dy = dy * (dialog.data.flip_y and -1 or 1)
+          dx = dx * (dialog.data.xinv and -1 or 1)
+          dy = dy * f * (dialog.data.yinv and -1 or 1)
           
-          local len = math.sqrt(((dx*dx) + (dy*dy)) * math.abs(strength))
-          local nx = dx * strength * 0.5 + 128
-          local ny = dy * strength * 0.5 + 128
-          local nz = (255-len)
-          preview_img:drawPixel(x,y,app.pixelColor.rgba(nx,ny,nz,a))
+          local len = math.sqrt(dx^2 + dy^2)
+          local w = lerp(1,len,s*0.9999)
+          local nx = (dx / w + 1) / 2
+          local ny = (dy / w + 1) / 2
+          local nz = lerp(1 - len,1,s)
+          preview_img:drawPixel(x,y,app.pixelColor.rgba(
+          nx*255,
+          ny*255,
+          nz*255,
+          255))
         end
       end
       sprite:newCel(
@@ -177,31 +198,50 @@ local function updatePreview()
   app:refresh()
 end
 
+
+
 dialog:slider{
   id="bias",
-  label="bias",
+  label="Bias",
   min=-1,
   max=1,
-  value=1,
+  value=0,
   onchange = updatePreview,
-}
-
-dialog:check{
-  id="flip_y",
-  text="Flip Y",
-  selected = true,
-  label="OpenGL / DirectX",
-  onclick = updatePreview,
 }
 
 dialog:slider{
-  id="strength",
-  label="Strength",
-  min=-100,
+  id="normalize",
+  label="Normalize",
+  min=0,
   max=100,
-  value=100,
+  value=95,
   onchange = updatePreview,
 }
+
+dialog:combobox({
+  id = "kernel",
+  label = "Kernel",
+  option = "Adjacents & Diagonals",
+  options = {"Adjacents","Adjacents & Diagonals", "Diagonals"}
+})
+
+dialog:newrow { always = false }
+dialog:check {
+  id = "xinv",
+  label = "Invert",
+  text = "X",
+  selected = false,
+  onclick = updatePreview
+}
+
+dialog:check {
+  id = "yinv",
+  text = "Y",
+  selected = false,
+  onclick = updatePreview
+}
+
+dialog:newrow { always = false }
 
 dialog:check{
   id="replace_layer",
@@ -209,14 +249,16 @@ dialog:check{
   onclick = updatePreview,
 }
 
+local function cancel()
+  sprite:deleteLayer(preview_layer)
+  app:refresh() -- some regions of the image dosent update
+  dialog:close()
+ end
+
 dialog:separator{}
 dialog:button{
   text = 'Cancel',
-  onclick = function()
-    sprite:deleteLayer(preview_layer)
-    app:refresh() -- some regions of the image dosent update
-    dialog:close()
-   end
+  onclick = cancel
 }
 
 dialog:button{
