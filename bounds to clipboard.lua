150e679dc273f461f60186143d2244f87a84259c
GN68 <81744903+GN68@users.noreply.github.com>
2025-09-01 11:34:13 +0800

    added header

--- bounds to clipboard.lua
+++ bounds to clipboard.lua
@@ -1,23 +1,24 @@
--- SelectionBoundsToClipboard.lua
+--[[______   __
+  / ____/ | / /  by: GNanimates / https://gnon.top / Discord: @gn68s
+ / / __/  |/ / name: Bounds to Clipboard
+/ /_/ / /|  /  desc: helps me automate setting UVs to my projects
+\____/_/ |_/ source: link ]]
 local spr = app.sprite
 if not spr then
-  return app.alert("No active sprite")
+	return app.alert("No active sprite")
 end
 
 local sel = spr.selection
 if sel.isEmpty then
-  return app.alert("No selection made")
+	return app.alert("No selection made")
 end
 
--- Bounds of the selection
 local b = sel.bounds
 local x1, y1 = b.x, b.y
 local x2, y2 = b.x + b.width - 1, b.y + b.height - 1
 
 local text = string.format("%d,%d,%d,%d", x1, y1, x2, y2)
 
--- Copy to clipboard
 app.clipboard.text = text
 
--- Optional alert (can remove if you want it silent)
 app.alert("Copied to clipboard:\n" .. text)
